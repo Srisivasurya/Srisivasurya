31a484ef956898e1523bfe56409245c1163765ff
Srisivasurya <srisivasurya777@gmail.com>
2021-05-31 17:58:56 +0530

    Java Test

---
+++ src/Test/citizen.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class citizens {
+    public static void main(String[] args){
+        int minor
+        Scanner user_input=new Scanner(System.in);
+        System.out.println("Enetr the size of array");
+        size=user_input.nextInt();
+        age=new int[size];
+        System.out.println("Enter the elements of array");
+        for(int i=0;i<age.length;i++)
+        {
+            age[i]=user_input.nextInt();
+        }
+
+    }
+}
