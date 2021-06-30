47203f2283b3b001ddeef83d9ae798e8c58dfad3
Srisivasurya <srisivasurya777@gmail.com>
2021-06-30 20:31:05 +0530

    Polymorphism and Inheritance Concept

---
+++ src/Run.java
@@ -0,0 +1,26 @@
+class A
+{
+     public void print()
+     {
+         System.out.println("Iam in class A");
+     }
+}
+
+class B extends A
+{
+    public void print()
+    {
+        System.out.println("Iam in class B");
+    }
+}
+
+class Ride
+{
+    public static void main(String[] args)
+    {
+        A a = new A();
+        a.print();
+
+        A b
+    }
+}
