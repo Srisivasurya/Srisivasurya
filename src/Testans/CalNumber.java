49387877859ab90f6abc1c39c4a34de206f5fe9f
Srisivasurya <srisivasurya777@gmail.com>
2021-07-22 18:26:47 +0530

    Oops,Collection framework and Exception Handling

---
+++ src/Testans/CalNumber.java
@@ -0,0 +1,43 @@
+package Testans;
+import java.util.Scanner;
+
+public class CalculateNumber extends Exception {
+    public double calculate(double a, double b, String operator) {
+        double answer = 0;
+        try {
+            if (operator.equals("+")) {
+                answer = a + b;
+            } else if (operator.equals("-")) {
+                answer = a - b;
+            } else if (operator.equals("*")) {
+                answer = a * b;
+            } else if (operator.equals("/")) {
+                answer = a / b;
+            } else {
+                answer = 0;
+            }
+            return answer;
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("The Exception is occurs : "+ e);
+        }
+
+        return answer;
+    }
+    public static void main(String args[]) {
+        CalculateNumber calculateNumber=new CalculateNumber();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first integer number");
+        double firstNumber=scan.nextDouble();
+        System.out.println("Enter the second integer number");
+        double secondNumber=scan.nextDouble();
+        Scanner scan1=new Scanner(System.in);
+        System.out.println("Enter the operator     Ex: +, _, *, /");
+        String operator=scan1.nextLine();
+
+        calculateNumber.calculate(firstNumber,secondNumber,operator);
+        double result =calculateNumber.calculate(firstNumber,secondNumber,operator);
+        System.out.println("The result is " + result);
+
+    }
+}
---
+++ src/Testans/Pattern.java
@@ -0,0 +1,31 @@
+package Testans;
+
+public class Pattern {
+    public static void main(String args[]) {
+        int count=5;
+        int j=0;
+        int rows=4;
+        int rw = 5;
+        int k=0;
+        for(int i=0;i<count;i++) {
+            System.out.print("* ");
+            for ( j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            for (int z= rw-1; z>=0 ; z--) {
+                for (k = 0; k <= rows; k++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+                /*if (k ==4)*/
+                if (k == rows + 1) {
+                    rows--;
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
